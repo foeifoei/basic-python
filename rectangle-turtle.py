fed65bfc86523692c3eb0964a00eb6efd167ea1a
foeifoei <110921560+foeifoei@users.noreply.github.com>
2022-08-09 22:40:53 +0700

    uploadfilenongtao

---
+++ rectangle-turtle.py
@@ -0,0 +1,53 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.right(270)
+
+        
+Rectangle()
+tao.right(180)
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.left(270)
+
+        
+Rectangle()
+tao.right(180)
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.right(270)
+
+        
+Rectangle()
+tao.forward(200)
+tao.right(90)
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.right(270)
+
+        
+Rectangle()
+tao.forward(100)
+def Rectangle() :
+    for i in range(3) :
+        tao.right(90)
+        tao.forward(100)
+
+        
+Rectangle()
+tao.left(90)
+tao.penup()
+tao.forward(50)
+tao.left(90)
+tao.right(180)
+tao.forward(200)
+tao.right(90)
+tao.forward(250)
