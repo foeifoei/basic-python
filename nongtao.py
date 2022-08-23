9709a6e30c22668124c289f106b5a81953078cf2
Foeifoei <110921560+foeifoei@users.noreply.github.com>
2022-08-23 17:00:28 +0700

    nongtaoja

---
+++ nongtao.py
@@ -0,0 +1,48 @@
+import turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.right(270)
+
+        
+Rectangle()
+tao.right(180)
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.left(270)
+
+        
+Rectangle()
+tao.right(180)
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.right(270)
+
+        
+Rectangle()
+tao.forward(200)
+tao.right(90)
+def Rectangle() :
+    for i in range(3) :
+        tao.forward(100)
+        tao.right(270)
+
+
+        
+Rectangle()
+tao.forward(100)
+def Rectangle() :
+    for i in range(3) :
+        tao.right(90)
+        tao.forward(100)
+
+Rectangle()
+tao.right(90)
+def Rectangle() :
+    for i in range(3) :
+        tao.right(90)
+        tao.forward(270)
